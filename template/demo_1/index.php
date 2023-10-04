8b5e6dfafcf728bba60bd1b6b5583d883453eeaf
Jooollyyyy <paredesjoelybercpe1996@gmail.com>
2023-10-04 22:27:17 +0800

    ukhki

--- template/demo_1/index.php
+++ template/demo_1/index.php
@@ -90,9 +90,6 @@
         <!-- partial:partials/_navbar.html -->
         <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
           <div class="navbar-menu-wrapper d-flex align-items-stretch">
-            <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-              <span class="mdi mdi-chevron-double-left"></span>
-            </button>
             <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
               <a class="navbar-brand brand-logo-mini" href="index.php"><img src="../assets/images/logo-mini.svg" alt="logo" /></a>
             </div>
--- template/demo_1/patientrecord.php
+++ template/demo_1/patientrecord.php
@@ -88,9 +88,6 @@
         <!-- partial:partials/_navbar.html -->
         <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
           <div class="navbar-menu-wrapper d-flex align-items-stretch">
-          <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-              <span class="mdi mdi-chevron-double-left"></span>
-            </button>
           <ul class="navbar-nav navbar-nav-right">
               <li class="nav-item nav-logout d-none d-md-block">
                 <button class="btn btn-sm btn-danger">Logout</button>
