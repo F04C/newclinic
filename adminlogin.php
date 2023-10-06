7d932d8922404f24ff44b9130e5a1214a9e5d449
F04C <israeldarrel@yahoo.com>
2023-10-06 22:00:00 +0800

    added role checker

--- adminlogin.php
+++ adminlogin.php
@@ -6,42 +6,59 @@ if (isset($_POST['btnSignin'])) {
     $usernameInput = $_POST['inputUsername'];
     $userpassInput = $_POST['inputPassword'];
 
-    //SQL SYNTAX for checking username and password
-    $sql = "SELECT * FROM tbluser  where username = '" . $usernameInput . "' and password = '" . $userpassInput . "';";
-    //connect to the database server
-    if ($conn) {
+    // SQL syntax to retrieve user information, including the role
+    $sql = "SELECT u.*, r.isAdmin, r.isSec, r.isDoc
+            FROM tbluser AS u
+            JOIN tbluserroles AS r ON u.userid = r.userid
+            WHERE u.username = '" . $usernameInput . "' AND u.password = '" . $userpassInput . "';";
 
-        //execute the sql syntax
+    // Connect to the database server
+    if ($conn) {
+        // Execute the SQL query
         try {
             $executeSQL = mysqli_query($conn, $sql);
 
             if ($executeSQL) {
-                //check num of rows response
+                // Check the number of rows returned
                 $numRows = mysqli_num_rows($executeSQL);
 
                 if ($numRows == 1) {
-
-                    //fetch specific record
+                    // Fetch user information, including roles
                     $record = mysqli_fetch_assoc($executeSQL);
 
-                    //create sessions
+                    // Create sessions
                     session_start();
                     $_SESSION['username'] = $record['username'];
                     $_SESSION['userid'] = $record['iduser'];
 
+                    // Store user roles in the session
+                    $_SESSION['isAdmin'] = $record['isAdmin'];
+                    $_SESSION['isSec'] = $record['isSec'];
+                    $_SESSION['isDoc'] = $record['isDoc'];
 
-                    header('Location: index.php');
+                    // Redirect to the appropriate page based on user role
+                    if ($record['isAdmin'] == 1) {
+                        header('Location: admin_dashboard.php');
+                    } elseif ($record['isDoc'] == 1) {
+                        header('Location: doctor_dashboard.php');
+                    } elseif ($record['isSec'] == 1) {
+                        header('Location: secretary_dashboard.php');
+                    } else {
+                        // Handle other roles or scenarios as needed
+                        echo "Unknown user role!";
+                    }
                 } else {
                     echo "No user found!";
                 }
             } else {
-                echo "may sala?";
+                echo "Query execution error!";
             }
         } catch (Exception $e) {
             echo 'Error';
         }
     } else {
+        echo "Database connection error!";
     }
 } else {
-    echo "HINDI MAG SHORTCUT BALIK KA SA LOGIN PAGE";
+    header('Location: login.php');
 }
