c037cb585e68ff9389c4952b985c65eee193c9c1
Jooollyyyy <paredesjoelybercpe1996@gmail.com>
2023-10-19 17:03:04 +0800

    sadasdas

--- secappointment.php
+++ secappointment.php
@@ -1,5 +1,82 @@
 
+<div class="main-panel">
+        <div class="content-wrapper pb-0">
+          <div class="page-header flex-wrap">
+            <div class="header-left">
+              <button class="btn btn-primary mb-2 mb-md-0 me-2" data-toggle="modal" data-target="#patientModal">Create new record</button>
+            </div>
+            <div class="header-right d-flex flex-wrap mt-2 mt-sm-0">
+              <div class="d-flex align-items-center">
+                <input type="text" class="form-control border-0" placeholder="Search" />
+              </div>
+            </div>
+          </div>
+            <!-- Modal for adding patient information -->
+            <div class="modal fade" id="patientModal" tabindex="-1" role="dialog" aria-labelledby="patientModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="patientModalLabel">Add Patient Information</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
 
+
+                                <!-- Your patient information form here -->
+                                <div class="modal-body">
+                                    <fieldset class="custom-fieldset">
+                                        <legend>
+                                            <h1><b>Patient Information</b></h1>
+                                        </legend>
+                                        <div class="nameage">
+                                            <label for="firstName">First Name:</label>
+                                            <input class="form-control" type="text" id="firstName" name="fname" placeholder="First Name">
+                                        </div>
+
+                                        <div class="nameage"><br>
+                                            <label for="middleName">Middle Name:</label>
+                                            <input class="form-control" type="text" id="middleName" name="mname" placeholder="Middle Name">
+                                        </div>
+
+                                        <div class="nameage"><br>
+                                            <label for="lastName">Last Name:</label>
+                                            <input class="form-control" type="text" id="lastName" name="lname" placeholder="Last Name">
+                                        </div>
+                                        <div class="nameage"><br>
+                                            <label for="lastName">Age:</label>
+                                            <input class="form-control" type="text" id="lastName" name="lname" placeholder="Ex. 15">
+                                        </div>
+                                        <div class="nameage">
+                                            <label for="lastName">Sex:</label><br>
+                                            <input type="radio" name="sex" id="male" value="male" checked>
+                                            <label for="male">Male</label>
+                                            <input type="radio" name="sex" id="female" value="female">
+                                            <label for="female">Female</label>
+                                            <div class="nameage">
+                                                <label for="lastName">Civil Status:</label>
+                                                <input class="form-control" type="text" id="lastName" name="lname" placeholder="Ex.Single">
+                                            </div>
+                                            <div class="nameage"><br>
+                                                <label for="lastName">Address:</label>
+                                                <input class="form-control" type="text" id="lastName" name="lname" placeholder="Iloilo City">
+                                            </div>
+                                            <div class="nameage"><br>
+                                                <label for="lastName">Data Of Birth:</label><br>
+                                                <br>
+                                                <input type="date" id="dateOfBirth" name="pDOB">
+                                            </div>
+
+                                            <!-- Rest of your form fields -->
+                                    </fieldset>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 <div class="col-xl-12 stretch-card grid-margin">
     <div class="card">
         <div class="card-body pb-0">
